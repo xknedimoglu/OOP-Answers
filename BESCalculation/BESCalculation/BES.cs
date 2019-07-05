841bfc78b8e0c3553f8f327d5506056711f234df
xknedimoglu <46479100+xknedimoglu@users.noreply.github.com>
2019-07-05 13:06:37 +0300

    Add files via upload

---
+++ BESCalculation/BESCalculation/BES.cs
@@ -0,0 +1,102 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace BESCalculation
+{
+    class BES
+    {
+        //2 tanesi 3.yü hesaplayacak ve bu hesaplandığı anda, başka 2 attribute ın hesaplanmasına sebep olacak
+        private string isim;
+        private string soyisim;
+        private string tcKimlikNo;
+        private string doğumYeri;
+        private string bankaİsmi;
+        private double toplamKazanç;
+        private double primeEsasKazançYüzdesi;
+        private double aylıkBesÖdemeMiktarı;
+        private int sistemeGirişYılı;
+        private int sistemdenÇıkışYılı;
+        private double devletDestekMiktarı;
+        private int sistemdeKaldığıSüre;
+
+        
+
+        public string getİsim() {
+            return isim;
+        }
+        public void setİsim(string isim) {
+            this.isim = isim;
+        }
+        public string getSoyisim() {
+            return soyisim;
+        }
+        public void setSoyisim(string soyisim) {
+            this.soyisim = soyisim;
+        }
+        public string getTcKimlikNo() {
+            return tcKimlikNo;
+        }
+        public void setTcKimlikNo(string tcKimlikNo) {
+            this.tcKimlikNo = tcKimlikNo;
+        }
+        public string getDoğumYeri(){
+            return doğumYeri;
+        }
+        public void setDoğumYeri(string doğumYeri) {
+            this.doğumYeri = doğumYeri;
+        }
+        public string getBankaİsmi() {
+            return bankaİsmi;
+        }
+        public void setBankaİsmi(string bankaİsmi) {
+            this.bankaİsmi = bankaİsmi;
+        }
+        public double getToplamKazanç() {
+            return toplamKazanç;
+        }
+        public void setToplamKazanç(double toplamKazanç) {
+            this.toplamKazanç = toplamKazanç;
+        }
+        public double getPrimeEsasKazançYüzdesi() {
+            return primeEsasKazançYüzdesi;
+        }
+        public void setPrimeEsasKazançYüzdesi(double primeEsasKazançYüzdesi) {
+            this.primeEsasKazançYüzdesi = primeEsasKazançYüzdesi;
+        }
+        public double getAylıkBesÖdemeMiktarı() {
+            return aylıkBesÖdemeMiktarı;
+        }
+        public void setAylıkBesÖdemeMiktarı(double aylıkBesÖdemeMiktarı) {
+            this.aylıkBesÖdemeMiktarı = aylıkBesÖdemeMiktarı;
+        }
+        public int getSistemeGirişYılı(){
+            return sistemeGirişYılı;
+        }
+        public void setSistemeGirişYılı(int sistemeGirişYılı) {
+            this.sistemeGirişYılı = sistemeGirişYılı;
+        }
+        public int getSistemdenÇıkışYılı()
+        {
+            return sistemdenÇıkışYılı;
+        }
+        public void setSistemdenÇıkışYılı(int sistemendenÇıkışYılı)
+        {
+            this.sistemdenÇıkışYılı = sistemendenÇıkışYılı;
+        }
+        public double getDevletDestekMiktarı() {
+            return devletDestekMiktarı;
+        }
+        public void setDevletDestekMiktari(double devletDestekMiktarı) {
+            this.devletDestekMiktarı = devletDestekMiktarı;
+        }
+
+        public int getSistemdeKaldığıSüre() {
+            return sistemdeKaldığıSüre;
+        }
+        public void setSistemdeKaldığıSüre(int sistemdeKaldığıSüre){
+            this.sistemdeKaldığıSüre = sistemdeKaldığıSüre;
+        }
+
+    }
+}
---
+++ BESCalculation/BESCalculation/Program.cs
@@ -0,0 +1,98 @@
+﻿using System;
+
+namespace BESCalculation
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            BES bes = new BES();
+
+            bes.setİsim("KAAN");
+            string isim= bes.getİsim();
+
+            bes.setSoyisim("NEDİMOĞLU");
+            string soyisim=bes.getSoyisim();
+
+            bes.setTcKimlikNo("33466363846");
+            string tcKimlikNo=bes.getTcKimlikNo();
+
+            bes.setDoğumYeri("DİYARBAKIR");
+            bes.getDoğumYeri();
+
+            bes.setBankaİsmi("GARANTİ");
+            bes.getBankaİsmi();
+
+            bes.setSistemeGirişYılı(2019);
+            int girişYılı=bes.getSistemeGirişYılı();
+
+            bes.setSistemdenÇıkışYılı(2025);
+            int çıkışYılı=bes.getSistemdenÇıkışYılı();
+
+            bes.setToplamKazanç(4120);
+            double besToplamKazanç = bes.getToplamKazanç();
+
+            bes.setPrimeEsasKazançYüzdesi(3);
+            double primeEsasKazançYüzdesi = bes.getPrimeEsasKazançYüzdesi();
+
+            // çıkış yılı ve giriş yılı bilgisi ile sistemde kaldığı süre attribute değeri bulnurbulunur 
+            int sistemdeKaldığıSüre = çıkışYılı - girişYılı;
+
+            // hesaplanan sistemde kalma süresi değeri bulunmuş olur.
+            bes.setSistemdeKaldığıSüre(sistemdeKaldığıSüre);
+            bes.getSistemdeKaldığıSüre();
+            
+            if (sistemdeKaldığıSüre>3 && sistemdeKaldığıSüre<6)
+            {
+                // aylık bes ödeme Miktarı ve toplam kazanç da sistemde Kaldığı süreye istinaden hesaplanır.
+                double aylıkBesÖdemeMiktarı = besToplamKazanç * primeEsasKazançYüzdesi / 100;
+                bes.setAylıkBesÖdemeMiktarı(aylıkBesÖdemeMiktarı);
+
+                double toplamKazanç = (aylıkBesÖdemeMiktarı * sistemdeKaldığıSüre) + (((aylıkBesÖdemeMiktarı * sistemdeKaldığıSüre) * 15) / 100);
+                bes.setToplamKazanç(toplamKazanç);
+                bes.getToplamKazanç();
+
+                Console.WriteLine("Aylık BES ödeme miktarı " + aylıkBesÖdemeMiktarı + " olan " + isim + soyisim + " ve tc'si " + tcKimlikNo + " 'suna sahip ve  " + " sistemde kaldığı süre " + sistemdeKaldığıSüre + " yıl " + "olup " + "toplam kazancı :" + toplamKazanç + " tl dir");
+            }
+            else if (sistemdeKaldığıSüre>=6 && sistemdeKaldığıSüre<10)
+            {
+                // aylık bes ödeme Miktarı ve toplam kazanç da sistemde Kaldığı süreye istinaden hesaplanır.
+
+                double aylıkBesÖdemeMiktarı = (besToplamKazanç * primeEsasKazançYüzdesi) / 100;
+                bes.setAylıkBesÖdemeMiktarı(aylıkBesÖdemeMiktarı);
+
+                double toplamKazanç = (aylıkBesÖdemeMiktarı * sistemdeKaldığıSüre) + (((aylıkBesÖdemeMiktarı * sistemdeKaldığıSüre) * 35) / 100);
+                bes.setToplamKazanç(toplamKazanç);
+                bes.getToplamKazanç();
+
+                Console.WriteLine("Aylık BES ödeme miktarı " + aylıkBesÖdemeMiktarı + " olan " + isim + soyisim + " ve tc'si " + tcKimlikNo + " 'suna sahip ve  " + " sistemde kaldığı süre " + sistemdeKaldığıSüre + " yıl " + "olup " + "toplam kazancı :"+ toplamKazanç + " tl dir");
+            }
+            else if (sistemdeKaldığıSüre>=10)
+            {
+                // aylık bes ödeme Miktarı ve toplam kazanç da sistemde Kaldığı süreye istinaden hesaplanır.
+
+                double aylıkBesÖdemeMiktarı = (besToplamKazanç * primeEsasKazançYüzdesi) / 100;
+                bes.setAylıkBesÖdemeMiktarı(aylıkBesÖdemeMiktarı);
+
+                double toplamKazanç = (aylıkBesÖdemeMiktarı * sistemdeKaldığıSüre) + (((aylıkBesÖdemeMiktarı * sistemdeKaldığıSüre) * 60) / 100);
+                bes.setToplamKazanç(toplamKazanç);
+                bes.getToplamKazanç();
+
+                Console.WriteLine("Aylık BES ödeme miktarı " + aylıkBesÖdemeMiktarı + " olan " + isim + soyisim + " ve tc'si " + tcKimlikNo + " 'suna sahip ve  " + " sistemde kaldığı süre " + sistemdeKaldığıSüre + " yıl " + "olup " + "toplam kazancı :" + toplamKazanç + " tl dir");
+            }
+            else
+            {
+                Console.WriteLine("BES sisteminde kazancınız bulunmamaktadır.");
+            }
+
+            Console.ReadLine();
+
+
+           
+
+
+
+
+        }
+    }
+}
