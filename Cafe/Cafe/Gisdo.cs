d20232ed208c7d3fe6640035ed1df33602eb96d2
xknedimoglu <46479100+xknedimoglu@users.noreply.github.com>
2019-07-05 12:56:05 +0300

    Add files via upload

---
+++ Cafe/Cafe/Gisdo.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Cafe
+{
+    class Gisdo
+    {
+        private string location;
+        private bool narghileOrNot;
+        private int numberOfEmployee;
+        private bool playingFieldOrNot;
+
+        public string getLocation() {
+            return location;
+        }
+        public void setLocation(string location) {
+            this.location = location;
+        }
+
+        public bool getNarghileOrNot() {
+            return narghileOrNot;
+        }
+        public void setNargileOrNot(bool narghileOrNot) {
+            this.narghileOrNot = narghileOrNot;
+        }
+        public int getNumberOfEmployees() {
+            return numberOfEmployee;
+        }
+        public void setNumberOfEmployee(int numberOfEmployee) {
+            this.numberOfEmployee = numberOfEmployee;
+        }
+        public bool getPlayingFieldOrNot() {
+            return playingFieldOrNot;
+        }
+        public void setPlayingFieldOrNot(bool playingFieldOrNot) {
+            this.playingFieldOrNot = playingFieldOrNot;
+        } 
+    }
+}
---
+++ Cafe/Cafe/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+
+namespace Cafe
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Gisdo gisdo = new Gisdo();
+            
+            gisdo.setNargileOrNot(true);
+            gisdo.setLocation("Ataşehir");
+            gisdo.setNumberOfEmployee(25);
+            gisdo.setPlayingFieldOrNot(true);
+
+            Console.WriteLine(gisdo.getNarghileOrNot());
+            Console.WriteLine(gisdo.getLocation());
+            Console.WriteLine(gisdo.getNumberOfEmployees());
+            Console.WriteLine(gisdo.getPlayingFieldOrNot());
+            Console.ReadLine();
+        }
+    }
+}
