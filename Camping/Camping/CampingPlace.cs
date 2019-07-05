2a9d4c9015349591bc1d4678d19fc832bd8ea1c5
xknedimoglu <46479100+xknedimoglu@users.noreply.github.com>
2019-07-05 13:00:55 +0300

    Add files via upload

---
+++ Camping/Camping/CampingPlace.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Camping
+{
+    class CampingPlace
+    {
+        private string name;
+        private string location;
+        private int sizeOfCampsite;
+        private double priceForEachTent;
+
+        public string getName() {
+            return name;
+        }
+        public void setName(string name) {
+            this.name = name;
+        }
+        public string getLocation()
+        {
+            return location;
+        }
+        public void setLocation(string location) {
+            this.location = location;
+        }
+        public int getSizeOfCampsite() {
+            return sizeOfCampsite;
+        }
+        public void setSizeOfCampsite(int sizeOfCampsite) {
+            this.sizeOfCampsite = sizeOfCampsite;
+        }
+        public double getPriceForEachTent() {
+            return priceForEachTent;
+        }
+        public void setPriceForEachTent(double priceForEachTent) {
+            this.priceForEachTent = priceForEachTent;
+        }
+    }
+}
---
+++ Camping/Camping/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+
+namespace Camping
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            CampingPlace campingPlace = new CampingPlace();
+
+            campingPlace.setName("Kaan's Camping");
+            campingPlace.setLocation("izmir");
+            campingPlace.setPriceForEachTent(50);
+            campingPlace.setSizeOfCampsite(400);
+
+            Console.WriteLine(campingPlace.getName());
+            Console.WriteLine(campingPlace.getLocation());
+            Console.WriteLine(campingPlace.getPriceForEachTent());
+            Console.WriteLine(campingPlace.getSizeOfCampsite());
+            Console.ReadLine();
+        }
+    }
+}
