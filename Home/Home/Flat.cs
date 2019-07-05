8e8405206d743231f0e44e7acb9c6bc0db4d48df
xknedimoglu <46479100+xknedimoglu@users.noreply.github.com>
2019-07-05 13:05:43 +0300

    Add files via upload

---
+++ Home/Home/Flat.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Home
+{
+    class Flat
+    {
+        private int floor;
+        private string numberOfRooms;
+        private string homeOwner;
+        private bool furnishedOrNot;
+        private int price;
+
+        public int getFloor() {
+            return floor;
+        }
+        public void setFloor(int floor) {
+            this.floor = floor;
+        }
+
+        public string getNumberOfRooms() {
+            return numberOfRooms;
+        }
+        public void setNumberOfRooms(string numberOfRooms) {
+            this.numberOfRooms = numberOfRooms;
+        }
+
+        public string gethomeOwner() {
+            return homeOwner;
+        }
+        public void setHomeOwner(string homeOwner) {
+            this.homeOwner = homeOwner;
+        }
+        public bool getFurnishedOrNot() {
+            return furnishedOrNot;
+        }
+        public void setFurnishedOrNot(bool furnishedOrNot) {
+            this.furnishedOrNot = furnishedOrNot;
+        }
+        public int getPrice() {
+            return price;
+        }
+        public void setPrice(int price) {
+            this.price = price;
+        }
+
+
+    }
+}
---
+++ Home/Home/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+
+namespace Home
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Flat flat = new Flat();
+
+            flat.setFloor(17);
+            flat.setFurnishedOrNot(true);
+            flat.setHomeOwner("Eşber");
+            flat.setNumberOfRooms("1+0");
+            flat.setPrice(900);
+
+            Console.WriteLine("FLAT 1");
+            Console.WriteLine(flat.getFloor());
+            Console.WriteLine(flat.getFurnishedOrNot());
+            Console.WriteLine(flat.gethomeOwner());
+            Console.WriteLine(flat.getNumberOfRooms());
+            Console.WriteLine(flat.getPrice());
+
+            Console.ReadLine();
+        }
+    }
+}
