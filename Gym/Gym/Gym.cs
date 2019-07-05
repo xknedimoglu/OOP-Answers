9ad2aea90baa41b339ba184725c36dbf3eaf48c9
xknedimoglu <46479100+xknedimoglu@users.noreply.github.com>
2019-07-05 13:03:25 +0300

    Add files via upload

---
+++ Gym/Gym/Gym.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Gym
+{
+    class Gym
+    {
+        private string name;
+        private int registeredPeople;
+        private int numberOfSportImplements;
+        private string location;
+        private int numberOfWorkingTrainer;
+
+        public string getName() {
+            return name;
+        }
+        public void setName(string name) {
+            this.name = name;
+        }
+        public int getRegisteredPeople() {
+            return registeredPeople;
+        }
+        public void setRegisteredPeople(int registeredPeople)
+        {
+            this.registeredPeople = registeredPeople;
+        }
+        public int getNumberOfSportImplements() {
+            return numberOfSportImplements;
+        }
+        public void setNumberOfSportImplements(int numberOfSportImplements) {
+            this.numberOfSportImplements = numberOfSportImplements;
+        }
+        public string getLocation()
+        {
+            return location;
+        }
+        public void setLocation(string location) {
+            this.location = location;
+        }
+        public int getNumberOfWorkingTrainer() {
+            return numberOfWorkingTrainer;
+        }
+        public void setNumberOfWorkingTrainer(int numberOfWorkingTrainer) {
+            this.numberOfWorkingTrainer = numberOfWorkingTrainer;
+        }
+
+    }
+}
---
+++ Gym/Gym/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+
+namespace Gym
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Gym gym = new Gym();
+
+            gym.setName("MACFIT");
+            gym.setLocation("Ataşehir");
+            gym.setNumberOfSportImplements(80);
+            gym.setNumberOfWorkingTrainer(25);
+            gym.setRegisteredPeople(1000);
+
+
+            Console.WriteLine(gym.getName());
+            Console.WriteLine(gym.getLocation());
+            Console.WriteLine(gym.getNumberOfSportImplements());
+            Console.WriteLine(gym.getNumberOfWorkingTrainer());
+            Console.WriteLine(gym.getRegisteredPeople());
+            Console.ReadLine();
+        }
+    }
+}
